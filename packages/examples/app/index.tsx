c1f76bae389404058f34b32a89f813b2334681e2
dallasclark <dallasjclark@gmail.com>
2024-07-29 15:08:31 +1000

    WIP

--- packages/examples/app/index.tsx
+++ packages/examples/app/index.tsx
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import { multiply } from 'reactnativeui';
 
 export default function Index() {
+  const [result, setResult] = useState<number | undefined>();
+
+  useEffect(() => {
+    multiply(3, 7).then(setResult);
+  }, []);
+
   return (
     <View
       style={{
@@ -11,6 +18,8 @@ export default function Index() {
       <Text>React Native UI Examples</Text>
 
       <Text>Use the top-left navigation bar to browse components.</Text>
+
+      <Text>Result: {result}</Text>
     </View>
   );
 }

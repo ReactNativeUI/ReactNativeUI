96d55824104a31e2c1ceaddd260b4e476b8662fc
dallasclark <dallasjclark@gmail.com>
2024-07-29 16:23:57 +1000

    WIP

--- packages/reactnativeui/src/components/Text.tsx
+++ packages/reactnativeui/src/components/Text.tsx
@@ -1,5 +1,13 @@
 import { Text as RNText } from 'react-native';
 
 export default function Text() {
-  return (<><RNText>Testing</RNText><RNText>This is going to be a very long string that should be separated onto a new line</RNText></>);
+  return (
+    <>
+      <RNText>Testing</RNText>{' '}
+      <RNText>
+        This is going to be a very long string that should be separated onto a
+        new line
+      </RNText>
+    </>
+  );
 }

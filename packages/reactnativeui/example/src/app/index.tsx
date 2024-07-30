c84ad0d175d3f739ef9c163f1407e2ea142af381
dallasclark <dallasjclark@gmail.com>
2024-07-30 15:24:25 +1000

    WIP

--- packages/reactnativeui/example/src/app/index.tsx
+++ packages/reactnativeui/example/src/app/index.tsx
@@ -3,7 +3,7 @@ import { H1, Text, View } from 'reactnativeui';
 export default function Index() {
   return (
     <View>
-      <H1>React Native UI Examples</H1>
+      <H1>React Native UI</H1>
 
       <Text>Use the top-left navigation bar to browse components.</Text>
     </View>
